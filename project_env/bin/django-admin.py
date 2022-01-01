17c45cb96129521eacb35f152c58b1383c4df045
BisaiShanku <shankubisai3333@gmail.com>
2022-01-02 01:16:52 +0530

    My Second commit

---
+++ project_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/shankus/Desktop/My Study Backup Latest/Python Development/Build a Backend RestAPI with Python and Django-Beginner/project_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
